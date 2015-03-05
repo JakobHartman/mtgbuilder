59f564c824ec91348ca94a32f2174949052a5dfe
Jakob <Jakobhartman@hotmail.com>
2015-03-04 19:24:57 -0700

    login finished

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,8 +1,9 @@
 
 (function($){
-		
+	var ref = new Firebase("https://mtgdeckpricerweb.firebaseio.com/");
+	
 	$(document).ready(function(){
-
+		var userEmail = localStorage["user"]
 		$('input.box-text').bind('focus blur', function(){
 			$(this).toggleClass('focus');
 		});
@@ -14,8 +15,39 @@
 			$(this).find('.overlay').hide();
 			$(this).find('.overlay').next().css({'opacity': 1});
 		});
-
+		
+		$("#loginSubmit").bind("click",login)
+		
+		
 	});
+
+	function login(){
+		var checked = $("#checkToRemember").attr('checked')
+		var session = "default"
+		if(!checked){
+			session = "sessionOnly"
+		}
+		console.log(session)
+		ref.authWithPassword({
+				email : $("#loginUsername").val(),
+				password : $("#loginPassword").val(),
+				remember : session
+			},authDataCallback)
+	}
+
+
+
+	function authDataCallback(error,authData) {
+  		if (authData) {
+			console.log("User " + authData.uid + " is logged in with " + authData.provider);
+			window.location.replace("index.html");
+  		} else {
+    		console.log(error.code);
+    		//window.location.replace("index.html");
+  		}
+	}
 
 
 })(jQuery);
+
+
